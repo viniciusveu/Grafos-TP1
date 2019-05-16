8f90632a73029480f462358dece0148743c234ae
João Baptista <joaosbaptista@hotmail.com>
2019-05-16 01:49:58 -0300

    leitura dos arquivos e mapeamento

--- Grafos-TP1/reader.js
+++ Grafos-TP1/reader.js
@@ -1,33 +1,35 @@
-const readText = (id) =>{
-    let string = $(id).val();
-    let stringArray = [];
-    let finalArray = [];
-    for (let i = 0; i < string.length; i++)
-        stringArray.push(string[i].trim()); //separou 60 em 6 e 0
-    for (let i = 0; i < stringArray.length; i++){
-        if(stringArray[i] != "") {
-            finalArray.push(stringArray[i]); 
-        }
-    }
-    console.log(finalArray);
-    createMatrizAdj(finalArray);
-}
-
 const createMatrizAdj = (array) => {
-    let def   = array[0];
-    let size  = array[1];
+    let def = array[0];
+    let size = array[1];
     let grafo = [];
     let index = 2
-    while(index < array.length - 3) {        
-        let i    = array[index];
-        let j    = array[index+1];
-        let peso = array[index+2]
+    while (index < array.length - 3) {
+        let i = array[index];
+        let j = array[index + 1];
+        let peso = array[index + 2]
         console.log('i = ' + i);
         console.log('j = ' + j);
         console.log('peso = ' + peso);
         index += 3;
     }
 }
+
+const input = document.getElementById('txtToRead');
+console.log(input);
+
+input.addEventListener('change', (e) => {
+    console.log(input.files);
+    const reader = new FileReader();
+    reader.onload = () => {
+        const lines = reader.result.split('\n').map((line) => { 
+            return line.split(' ');
+        });
+        console.log(lines);
+        
+    }
+    reader.readAsText(input.files[0]);
+}, false);
+
 
 /*
 0
@@ -38,5 +40,4 @@ const createMatrizAdj = (array) => {
 2 3 4
 3 1 10
 4 2 15
-(24) [,, "0", "2", "4", "0", "4", "6", "0", "0", "3", "2", "3", "2", "3", "4", "3", "1", "1", "0", "4", "2", "1", "5"]
  */
